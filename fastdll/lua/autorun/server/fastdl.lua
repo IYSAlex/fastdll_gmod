74c600f867e81d8d174dbd0ebf448f81ccdd6cc5
Alex <90734106+IYSAlex@users.noreply.github.com>
2021-09-15 17:32:41 -0400

    Add files via upload

---
+++ fastdll/lua/autorun/server/fastdl.lua
@@ -0,0 +1,34 @@
+timer.Simple(0.1, function() resource.AddWorkshopCollection("2546051317") end)
+
+function resource.AddWorkshopCollection( id )
+
+
+    local count = 0
+    local files = {}
+    
+	http.Fetch( "http://steamcommunity.com/sharedfiles/filedetails/?id=" .. id, function( page )	
+
+        
+	
+		for k in page:gmatch( [[<div id="sharedfile_(.-)" class="collectionItem">]] ) do
+			resource.AddWorkshop( k )
+            count = count + 1
+            
+            --Generator
+            
+            local files2 = "resource.AddWorkshop("..'"'..k..'"'..")"
+            
+            table.insert(files, files2)            
+            
+            
+		end
+        
+        
+            local fileNames = table.concat(files, "\n")
+		
+            print(fileNames)
+            file.Write( "WorkshopGenerator.txt", fileNames )
+            print("Workshop Files are: "..count)
+        
+	end )
+end
